52bc4e1c64fad924cf1bb09ce6b8b8358c8bf483
Gabriel <ggavinowich@gmail.com>
2021-08-03 22:42:05 -0300

    Calcular costo sin  impuestos ni descuentos
    Calcular el costo total sin considerar el impuesto y los descuentos

--- factura.rb
+++ factura.rb
@@ -1,13 +1,18 @@
 class Factura
     
     def initialize(cantidad, precio)
-        @cantidad = cantidad
-        @precio = precio
+        @cantidad = cantidad.to_f
+        @precio = precio.to_f
         puts "Cantidad ingresada es #{@cantidad}"
         puts "Precio ingresado es #{@precio}"
     end
 
+    def calcular_precio
+        @costo_fob = @cantidad * @precio
+        puts "El costo sin impuesto ni descuento es #{@costo_fob}"
+    end
 
 end
 
 factura = Factura.new ARGV[0], ARGV[1]
+factura.calcular_precio
