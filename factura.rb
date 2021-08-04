b47994c3ea17787f90a230d0e4add129c8880d6d
Gabriel <ggavinowich@gmail.com>
2021-08-03 22:36:27 -0300

    Leer precio
    Leer el precio unitario recibida por linea de comando e informarselo al usuario

--- factura.rb
+++ factura.rb
@@ -1,11 +1,13 @@
 class Factura
     
-    def initialize(cantidad)
+    def initialize(cantidad, precio)
         @cantidad = cantidad
+        @precio = precio
         puts "Cantidad ingresada es #{@cantidad}"
+        puts "Precio ingresado es #{@precio}"
     end
 
 
 end
 
-factura = Factura.new ARGV[0]
+factura = Factura.new ARGV[0], ARGV[1]
